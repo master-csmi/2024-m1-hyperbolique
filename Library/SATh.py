74cce0912d1da57d2d8197ec0b754f5417c225c7
Antoine Regardin <antoine.regardin@orange.fr>
2024-06-14 11:44:21 +0200

    cleaning

--- Library/SATh.py
+++ Library/SATh.py
@@ -23,10 +23,12 @@ class Problem():
         "Theta_min":0.2,
         "Theta_max":1,
         "Scheme": "SATh", #"Theta"
-        "Theta_computation":"Fixed-Point", #"Newton"
+        "Theta_computation":"Newton", #"Fixed-Point" has been abandoned
         "Theta_choice_epsilon":1e-6,
+        "Theta_choice_method":"MinMax", #"Smoothing"
         "Method_convergence_epsilon":1e-6,
         "Timer":False,
+        "print_Newton_iter":False,
         "Path": "pictures",
         "Boundary":"constant"
         }
@@ -46,7 +48,8 @@ class Problem():
 
         if self.default_dict["Scheme"] == "Theta":  #Simple/Standard Theta Scheme
             self.theta = self.default_dict["Theta_st"]
-            self.sol_num = self.Theta_Scheme()
+            scheme = Theta_Scheme(self)
+            self.sol_num = scheme.solver()
             self.sol_exc = self.sol_ex()
         
         elif self.default_dict["Scheme"] == "SATh":  #Self-Adaptive Theta Scheme
@@ -63,18 +66,6 @@ class Problem():
     def sol_ex(self):
         return self.funcs.exact(self.mesh.nodes, self.params, self.tf)
 
-    def Theta_Scheme(self):
-        t = 0
-        u = self.funcs.init_sol.copy()
-        coef = self.mesh.dt*(1-self.theta)
-        A = self.mats.Iter_Mat(self.mesh, self.theta, self.alpha, adaptive=False)
-        
-        while (t<self.tf):
-            t += self.mesh.dt
-            b = u - coef*self.alpha*(self.mats.Dx @ u)
-            u, _ = gmres(A, b)
-
-        return u
 
 
 class SATh_Solver:     #Works only for nonperiodical boundary conditions!
@@ -95,10 +86,6 @@ class SATh_Solver:     #Works only for nonperiodical boundary conditions!
                                            #where a=1
         #self.cyclicpermut_mat = sparse.csr_matrix(np.roll(np.eye(env.mesh.Nx), shift=-1, axis=0))
         #self.d = lambda u: self.env.funcs.arrange_u(u,-1,bc=self.default_dict["Boundary"])
-
-    def interpol_quadrature(self):  #use with tau=1/2 for standard linear interpolation
-        #return ((self.env.mesh.dt/2)*self.u_down + (1 - self.env.mesh.dt/2)*self.u_up)/self.env.mesh.dt
-        return self.u_down/2 + self.u_up/2
     
     def timer(self, keyword=None):
         if keyword == "set":
@@ -106,15 +93,23 @@ class SATh_Solver:     #Works only for nonperiodical boundary conditions!
         else:
             print("-",end="")
     
-    def theta_choice(self, v, w, epsilon=1e-6):
-        if np.abs(w) > epsilon:
-            #print(np.abs(v/w > 1))
-            #if np.abs(v/w) > self.theta_min:
-            #    print("theta=v/w",self.env.default_dict["Theta_computation"])
-            #    print(max(self.theta_min, np.abs(v/w) ))
-            return min(max(self.theta_min, np.abs(v/w) ), 1)  #
-        else:
-            return self.theta_st
+    def theta_choice(self, v, w, i, epsilon=1e-6):
+        if self.env.default_dict["Theta_choice_method"] == "MinMax":
+            if np.abs(w) > epsilon:
+                #print(np.abs(v/w > 1))
+                #if np.abs(v/w) > self.theta_min:
+                #    print("theta=v/w",self.env.default_dict["Theta_computation"])
+                #    print(max(self.theta_min, np.abs(v/w) ))
+                #return min(max(self.theta_min, np.abs(v/w) ), 1)  #
+                self.thetas[i] = min(max(self.theta_min, np.abs(v/w) ), 1)
+            else:
+                self.thetas[i] = self.theta_st
+        
+        elif self.env.default_dict["Theta_choice_method"] == "Smoothing":
+            self.thetas[i] = .75 + .25 * np.tanh(100 * (-.5 + v/w))
+        
+        else :
+            print("Wrong Theta choice method type")
     
 
     def F(self, w, v, i):
@@ -124,17 +119,25 @@ class SATh_Solver:     #Works only for nonperiodical boundary conditions!
         ])
 
     def compute_J(self, w, v, i, epsilon=1e-6):    #Compute the Jacobian
-        if np.abs(w[i])>epsilon :
-            if v[i]/w[i] > self.theta_min:
-                J = np.array([[1, self.lam], [-(v[i]**2)*self.lam / (2*(w[i]**2)), 1 + self.lam/w[i]]])
+        if self.env.default_dict["Theta_choice_method"] == "MinMax":
+            if np.abs(w[i])>epsilon :
+                if v[i]/w[i] > self.theta_min:
+                    J = np.array([[1, self.lam], [-(v[i]**2)*self.lam / (2*(w[i]**2)), 1 + self.lam/w[i]]])
+                else :
+                    J = np.array([[1+self.lam*self.theta_min, 0], [self.lam * (self.theta_min**2) / 2, 1]])
             else :
-                J = np.array([[1+self.lam*self.theta_min, 0], [self.lam * (self.theta_min**2) / 2, 1]])
-        else :
-            J = np.array([[1+self.lam*self.theta_st, 0], [self.lam * (self.theta_st**2) / 2, 1]])
+                J = np.array([[1+self.lam*self.theta_st, 0], [self.lam * (self.theta_st**2) / 2, 1]])
+
+        if self.env.default_dict["Theta_choice_method"] == "Smoothing":
+            J = np.array([[1 - 25 * v[i] * self.lam * np.sech(100*(-.5+ v[i]/w[i]))**2 / w[i] + self.lam * (3/4 + (1/4)*np.tanh(100*(-.5 + v[i]/w[i]))) ,
+                           25 * self.lam * np.sech(100*(-.5 + v[i]/w[i]))**2],
+                          [-(25 * v[i] * self.lam * np.sech(100*(-.5 + v[i]/w[i]))**2 * (3/4 + (1/4)*np.tanh(100*(-.5 + v[i]/w[i])))) / w + .5 * self.lam * (3/4 + (1/4)*np.tanh(100 * (-.5 + v[i]/w[i])))**2 ,
+                           1 + 25 * self.lam * np.sech(100 * (-.5 + v[i]/w[i]))**2 * (3/4 + (1/4)*np.tanh(100 * (-.5 + v[i]/w[i])))]])
+
         return J
 
 
-    def Newton(self, epsilon=1e-6):
+    def Newton(self, epsilon=1e-6, maxiter=5e2):
         w_ = np.copy(self.u_down)
         w_[0] = 0
         #v = np.empty_like(self.u_down)
@@ -142,55 +145,62 @@ class SATh_Solver:     #Works only for nonperiodical boundary conditions!
         v[0] = 0 #
         w = np.zeros_like(w_)
         #(Neumann at the left)
+        iter=0
 
-        while np.linalg.norm(w - w_) > epsilon:
+        while np.linalg.norm(w - w_) > epsilon and maxiter>iter:
             for i in range(1,self.env.mesh.Nx +1):
                 w_[i] = w[i]
 
-                self.thetas[i] = self.theta_choice(v[i],w[i],epsilon=self.env.default_dict["Theta_choice_epsilon"])
-
+                #update thetas 
+                self.theta_choice(v[i],w[i],i, epsilon=self.env.default_dict["Theta_choice_epsilon"])
                 #compute step (linear system resolution)
                 s = np.linalg.solve(self.compute_J(w,v,i, epsilon=self.env.default_dict["Theta_choice_epsilon"]), -self.F(w,v,i))
                 #iterate Newton problem
                 w[i] += s[0]
                 v[i] += s[1]
+            iter += 1
         
-        return [w,v]
+        if self.env.default_dict["print_Newton_iter"]==True:
+            print(iter, end=" ")
+
+        for i in range(w.size):
+            self.theta_choice(v[i],w[i],i, epsilon=self.env.default_dict["Theta_choice_epsilon"])
+        self.u_til = v
 
 
+    """
     def fixed_point_iter(self, theta_left, u_down_left, u_down,
-                                      w_left, lam, epsilon=1e-6):
+                                      w_left, i, epsilon=1e-6):
         w = w_left #
         w_ = epsilon + w  #Just to initialize
         theta = self.theta_st   #Try wit hthe previous time step theta
 
         while np.abs(w_ - w) >= epsilon:
             w_ = w
-            w = (u_down - theta_left*w_left - (u_down_left + w_left)) / (1 + lam*theta)
-            v = (-lam/2) * (u_down - theta_left**2 * w_left - (u_down_left + w_left) + theta**2*w)
-            theta = self.theta_choice(v,w, epsilon=self.env.default_dict["Theta_choice_epsilon"])
+            w = (u_down - theta_left*w_left - (u_down_left + w_left)) / (1 + self.lam*theta)
+            v = (-self.lam/2) * (u_down - theta_left**2 * w_left - (u_down_left + w_left) + theta**2*w)
+            self.theta_choice(v,w,i, epsilon=self.env.default_dict["Theta_choice_epsilon"])
+            theta = self.thetas[i]
         
         return theta, w
+    """
         
 
     def update_thetas(self):
 
-        if self.theta_computation == "Fixed-Point":
+        if self.theta_computation == "Newton":
+            self.Newton(epsilon=self.env.default_dict["Method_convergence_epsilon"])
+
+        """
+        elif self.theta_computation == "Fixed-Point":
             #self.u_down[0] = self.left_bound_val
             #w_left = self.u_up[0] - self.u_down[0]  #Boundary condition: the value at the left is constant
             w_left = 0  #For now, we work with a constant flux at the left, so the first w_left is equal to 0
 
             for i in range(1, self.env.mesh.Nx +1):
                 theta, w_left = self.fixed_point_iter(self.thetas[i-1], self.u_down[i-1],
-                                    self.u_down[i], w_left, self.lam, epsilon=self.env.default_dict["Method_convergence_epsilon"])
-                self.thetas[i] = theta
-
-        elif self.theta_computation == "Newton":
-            [w,v] = self.Newton(epsilon=self.env.default_dict["Method_convergence_epsilon"])
-            for i in range(w.size):
-                self.thetas[i] = self.theta_choice(v[i],w[i], epsilon=self.env.default_dict["Theta_choice_epsilon"])
-
-            self.u_til = v
+                                    self.u_down[i], w_left, i, epsilon=self.env.default_dict["Method_convergence_epsilon"])
+        """
 
 
     def SATh_Scheme(self):
@@ -223,7 +233,6 @@ class SATh_Solver:     #Works only for nonperiodical boundary conditions!
             t += self.env.mesh.dt
             if self.env.default_dict["Timer"]==True:
                 self.timer()
-                #print(t)
 
         return self.u_up
 
